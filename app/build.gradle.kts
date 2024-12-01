433fa57d91ff48380da7ef8ea2442e41652f0084
yu-ko-ba <59269129+yu-ko-ba@users.noreply.github.com>
2024-12-01 17:43:37 +0900

    update: build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,18 +13,19 @@ android {
         minSdk = 21
         targetSdk = 35
         versionCode = 1
-        versionName = "1.0"
+        versionName = "0.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            signingConfig = signingConfigs.getByName("debug")
         }
     }
     compileOptions {
